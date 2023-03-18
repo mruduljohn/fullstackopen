2bd26e6fc012bbb79758741422a3055fb35153cd
Mrudul John Mathews <67854062+mruduljohn@users.noreply.github.com>
2023-03-19 00:02:14 +0530

    Update App.js
    1.6

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -6,9 +6,28 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const  handleGoodClick = () => {
+    setGood(good + 1)
+  }
+
+  const  handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
+
   return (
     <div>
-      code here
+      <h1>give feedback</h1>
+      <button onClick={handleGoodClick}>good</button>
+      <button onClick={handleNeutralClick}>neutral</button>
+      <button onClick={handleBadClick}>bad</button>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
   )
 }
