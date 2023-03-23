bb1ea2ae0e772053712f981b3abd9fad8560381e
Mrudul John Mathews <67854062+mruduljohn@users.noreply.github.com>
2023-03-23 23:40:25 +0530

    json initial commit - 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState , useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({newFilter, handleFilterChange}) => {
   return (
@@ -40,19 +41,23 @@ const Persons = ({filteredpersons}) => {
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' , number: '040-123456' , id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNum, setNewNum] = useState('')
   const [newFilter, setNewFilter] = useState('')
 
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
+
+
   const addPerson = (event) => {
     event.preventDefault()
-    console.log('button clicked', event.target)
+    console.log('button click', event.target)
     const newPerson = { name: newName , number: newNum }
     if(persons.some(person => person.name === newName && person.number === newNum)) {
       alert(`${newName} is already added to phonebook with same number ${newNum}`)
