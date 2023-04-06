42f71c8e91449d8c328050772d41328a3e6ae96c
Mrudul John Mathews <67854062+mruduljohn@users.noreply.github.com>
2023-04-06 21:09:52 +0530

    Morgan Configured

--- part3/NOTES-BACKEND/index.js
+++ part3/NOTES-BACKEND/index.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms:body',
+    { skip: function (req, res) { return res.status >= 400 } 
+}))
 let persons = [
     { 
       "id": 1,
