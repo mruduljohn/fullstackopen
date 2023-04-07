179ce3b9c38d1801564cc073ff97f5b74c9824b1
Mrudul John Mathews <67854062+mruduljohn@users.noreply.github.com>
2023-04-07 23:45:01 +0530

    Folder Directory Change

--- part3/Phonebook_backend/index.js
+++ part3/Phonebook_backend/index.js
@@ -80,7 +80,7 @@ app.post('/api/persons', (req, res) => {
     res.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.port || 3001
 app.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}`)
 })
