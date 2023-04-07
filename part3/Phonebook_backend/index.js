58d1839a76543b4c3772650d508bb4cb935cd146
Mrudul John Mathews <67854062+mruduljohn@users.noreply.github.com>
2023-04-08 01:11:19 +0530

    number to num bug fixed

--- part3/Phonebook_backend/index.js
+++ part3/Phonebook_backend/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
-app.use(express.static('build'))
+//app.use(express.static('build'))
 app.use(express.json())
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms:body',
