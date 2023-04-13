5151ba127ad6f59d08740b7b8186fa650115dff2
Mrudul John Mathews <67854062+mruduljohn@users.noreply.github.com>
2023-04-13 23:49:04 +0530

    Person import

--- part3/Phonebook_backend/index.js
+++ part3/Phonebook_backend/index.js
@@ -10,6 +10,7 @@ morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms:body',
     { skip: function (req, res) { return res.status >= 400 } 
 }))
+/*
 let persons = [
     { 
       "id": 1,
@@ -32,62 +33,97 @@ let persons = [
       "number": "39-23-6423122"
     }
 ]
-
+*/
 app.get('/info', (req, res) => {
-    const date = new Date()
-    const info = `<p>Phonebook has info for ${persons.length} people</p>
-                  <p>${date}</p>` 
-    res.send(info)
+    const date = new Date().toString()
+    Person
+        .find({})
+        .then(persons => {
+            res.send(
+                `<p>Phonebook has info for ${persons.length} people</p>
+                <p>${date}</p>`
+            )
+        })
 })
 
 
 
 app.get('/api/persons', (req, res) => {
-  Person.find({}).then(persons => {
-    res.json(persons)
+  Person
+    .find({})
+    .then(persons => {
+        res.json(persons)
   })
 })
 
 app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const person = persons.find(person => {
-        //console.log(note.id, typeof note.id, id, typeof id,note.id===id)
-        return person.id === id})
-    if(person){
-        res.json(person)
-    }else{
-        res.status(404).end()
-    } 
-})
-app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(person => person.id !== id)
-  
-    res.status(204).end()
+    Person
+        .findById(req.params.id)
+        .then(person => {
+            if(person){
+                res.json(person)
+            }else{
+                res.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
-app.post('/api/persons', (req, res) => {
+app.delete('/api/persons/:id', (req, res,next) => {
+    Person
+        .findOneAndDelete({id: req.params.id})
+        .then(result => {
+            res.status(204).end()
+        })
+        .catch(error => next(error))
+})
+
+
+app.post('/api/persons', (req, res,next) => {
     const body = req.body
-    if (!body.name || !body.number) {
-        return res.status(400).json({
-            error: 'name or number missing'
-        })
-    }
 
-    const person = {
-        id : Math.floor(Math.random()*1000000),
+    const contact = new Person({
         name : body.name,
-        number : body.number
-    }
-    if (persons.find(p => p.name === person.name)) {
-        return res.status(400).json({
-            error: 'name must be unique'
+        number : body.number,
+    })
+    contact
+        .save()
+        .then(savedContact => savedContact.toJSON())
+        .then(savedAndFormattedContact => {
+            res.json(savedAndFormattedContact)
         })
-    }
-    
-    persons = persons.concat(person)
-    res.json(person)
+        .catch(error => next(error))
 })
+
+app.put('/api/persons/:id', (req, res, next) => {
+    const body = req.body
+    const contact = {
+        name : body.name,
+        number : body.number,
+    }
+    Person
+    .findByIdAndUpdate(req.params.id, contact, { new: true })
+    .then(updatedContact => {
+        res.json(updatedContact)
+    })
+    .catch(error => next(error))
+})
+
+const unknownEndpoint = (req,res) => {
+    res.status(404).send({error: 'unknown endpoint'})
+}
+app.use(unknownEndpoint)
+
+const errorHandler = (error, req, res, next) => {
+    console.error(error.message)
+    if(error.name === 'CastError'){
+        return res.status(400).send({error: 'malformatted id'})
+    }else if(error.name === 'ValidationError'){
+        return res.status(400).json({error: error.message})
+    }
+    next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.port || 3001
 app.listen(PORT,()=>{
