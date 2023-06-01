473d8d07936e21f206bd558cb68470e59c7c3f09
Mrudul John Mathews <67854062+mruduljohn@users.noreply.github.com>
2023-06-01 20:27:39 +0530

    favourite blog

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -9,9 +9,25 @@ const totalLikes = (blogs) => {
   
     return likes;
   }
+
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) {
+      return null;
+    }
   
+    const favorite = blogs.reduce((max, blog) => {
+      return blog.likes > max.likes ? blog : max;
+    }, blogs[0]);
+  
+    return {
+      title: favorite.title,
+      author: favorite.author,
+      likes: favorite.likes
+    };
+  }
   module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
   };
   
